c6767b5da7ae5e038a7f849abcb7080536b7642c
edenandom <edenandom@localhost>
2022-11-17 14:14:43 -0600

    finished

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -2,9 +2,60 @@ package studio8;
 
 public class Date {
 
+	private int month = 0;
+	private int day = 0;
+	private int year = 0;
+	private boolean isHoliday = false;
+	
+	public Date(int month, int day, int year, boolean isHoliday) {
+		super();
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.isHoliday = isHoliday;
+	}
 
+	public boolean isHoliday() {
+		return this.isHoliday;
+	}
+	
     public static void main(String[] args) {
+    	Date date1 = new Date (8, 2, 1998, true);
+    	System.out.println(date1.toString());
+    	Date date2 = new Date (11, 18, 2007, true);
+    	System.out.println(date2.toString());
+    	Date date3 = new Date (11, 17, 2007, true);
+    	System.out.println(date3.toString());
 
     }
+
+	@Override
+	public String toString() {
+		return "Date [month=" + month + ", day=" + day + ", year=" + year + ", isHoliday=" + isHoliday + "]";
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,10 +2,98 @@ package studio8;
 
 
 public class Time {
-
+	private int hour;
+	private int minute;
+	private boolean isMilitaryTime;
+	
 	public static void main(String[] args) {
- 
+		Time time1 = new Time(22, 30, false);
+		System.out.println(time1.toString());
+		Time time2 = new Time(15, 00, true);
+		System.out.println(time2.toString());
+		Time time3 = new Time(12, 56, true);
+		System.out.println(time3.toString());
+		Time time4 = new Time(11, 40, true);
+		System.out.println(time4.toString());
     	
     }
+	
+	/**
+	 * returns us a string containing our time.
+	 * @param N/A
+	 * @return String
+	 */
+	@Override
+	public String toString() {
+		if(!isMilitaryTime) {
+			return (hour-12) + ":" + minute;
+		} else {
+			return (hour) + ":" + minute;
+		}
+	}
+
+	/**
+	 * gives us the hour hand.
+	 * @param N/A
+	 * @return int
+	 */
+	public int getHour() {
+		return hour;
+	}
+
+	/**
+	 * sets the hour hand.
+	 * @param int hour
+	 * @return N/A
+	 */
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	/**
+	 * gives us the minute hand.
+	 * @param N/A
+	 * @return int
+	 */
+	public int getMinute() {
+		return minute;
+	}
+
+	/**
+	 * sets the minute hand.
+	 * @param int minute
+	 * @return N/A
+	 */
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+	/**
+	 * checks the format of the time. Returns true if military time.
+	 * @param N/A
+	 * @return boolean isMilitaryTime
+	 */
+	public boolean isMilitaryTime() {
+		return isMilitaryTime;
+	}
+
+	/**
+	 * sets true if military time.
+	 * @param boolean isMilitaryTime
+	 * @return N/A
+	 */
+	public void setMilitaryTime(boolean isMilitaryTime) {
+		this.isMilitaryTime = isMilitaryTime;
+	}
+
+
+	public Time(int hour, int minute, boolean isMilitaryTime) {
+		super();
+		this.hour = hour;
+		this.minute = minute;
+		this.isMilitaryTime = isMilitaryTime;
+	}
+	
+	
 
 }
